00284e985cf735520dfb96a2dd5527b447346cc1
zykoondev <developer@zykoontech.com>
2020-10-13 20:50:17 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+
+const PORT = process.env.PORT || 3000
+  
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+//set Template engine
+app.use(expressLayout)
+app.use('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+})
