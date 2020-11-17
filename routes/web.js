a2509663cdc970dda8d81343c480cc3a28baa32b
zykoondev <developer@zykoontech.com>
2020-11-17 12:21:15 +0530

    auth system

--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,22 @@
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartContoller')
 const homeController = require('../app/http/controllers/homeContoller')
+const guest = require('../app/http/middlewares/guest')
 
 function initRoutes(app){
     app.get('/', homeController().index)
 
     app.get('/cart', cartController().index)
 
-    app.get('/login', authController().login)
+    app.get('/login', guest, authController().login)
 
-    app.get('/register', authController().register)
+    app.post('/login', authController().postLogin)
+
+    app.get('/register', guest, authController().register)
+    
+    app.post('/register', authController().postRegister)
+
+    app.post('/logout', authController().logout)
 
     app.post('/update-cart', cartController().update)
     
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 //Database connection
 const url = "mongodb://localhost/pizza";
@@ -21,6 +22,7 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connection failed...')
 });
+
 
 //Session store
 let mongoStore = new MongoDbStore({
@@ -37,16 +39,24 @@ app.use(session({
     cookie: {maxAge: 1000 * 60 * 60 * 24} //24 hours
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.use(flash())
 
 //Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 //Global middleware
 app.use((req,res, next) => {
    res.locals.session = req.session
+   res.locals.user = req.user
    next()
 })
 
