311215962df4773470a229a8ec73095e2ff4bca0
zykoondev <developer@zykoontech.com>
2020-11-20 21:12:19 +0530

    add orders table, place order

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,14 @@
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartContoller')
 const homeController = require('../app/http/controllers/homeContoller')
+const orderController = require('../app/http/controllers/customers/orderController')
+const AdminOrderController = require('../app/http/controllers/admin/orderController')
+
+
+// Middlewares
 const guest = require('../app/http/middlewares/guest')
+const auth = require('../app/http/middlewares/auth')
+const admin = require('../app/http/middlewares/admin')
 
 function initRoutes(app){
     app.get('/', homeController().index)
@@ -20,6 +27,17 @@ function initRoutes(app){
 
     app.post('/update-cart', cartController().update)
     
+
+    //Customer routes
+    app.post('/orders', auth, orderController().store)
+    
+    app.get('/customer/orders', auth, orderController().index)
+
+
+    // Admin routes
+    app.get('/admin/orders', admin, AdminOrderController().index)
+
+
 }
 
 module.exports = initRoutes
